01d3f9ed5f41418020d1ae07c7aa8ca7f126c3bd
gsauarv <gsaurav2000@gmail.com>
2021-10-02 05:18:51 +0545

    Meta  tag added

--- Components/BlogsCards.js
+++ Components/BlogsCards.js
@@ -1,4 +1,4 @@
-import { Flex, Box, Text } from "@chakra-ui/react";
+import { Flex, Box, Text, Divider } from "@chakra-ui/react";
 const BlogsCards = ({
   blogTitle,
   publishedAt,
@@ -7,47 +7,50 @@ const BlogsCards = ({
   slug,
 }) => {
   return (
-    <Box shadow="xl" mt="20" py="10" borderRadius="xl">
-      <Flex
-        w={{ base: "100%", md: "100%", lg: "60%" }}
-        direction="column"
-        mb="10"
-        px="5"
-      >
-        <Text
-          letterSpacing="wider"
-          fontSize="xl"
-          fontWeight="bold"
-          textTransform="capitalize"
-          casing="lowercase"
-          letterSpacing="wide"
-          width={{ base: "100%", md: "100%", lg: "85%" }}
-          pb="3"
-          _hover={{
-            textDecoration: "underline",
-            textUnderlineOffset: 4,
-            cursor: "pointer",
-          }}
+    <>
+      <Box shadow="xl" mt="20" py="10" borderRadius="xl">
+        <Flex
+          w={{ base: "100%", md: "100%", lg: "60%" }}
+          direction="column"
+          mb="10"
+          px="5"
         >
-          {blogTitle}
-        </Text>
+          <Text
+            letterSpacing="wider"
+            fontSize="xl"
+            fontWeight="bold"
+            textTransform="capitalize"
+            casing="lowercase"
+            letterSpacing="wide"
+            width={{ base: "100%", md: "100%", lg: "85%" }}
+            pb="3"
+            _hover={{
+              textDecoration: "underline",
+              textUnderlineOffset: 4,
+              cursor: "pointer",
+            }}
+          >
+            {blogTitle}
+          </Text>
 
-        <Text
-          width="85%"
-          textAlign="justify"
-          pb="5"
-          color="gray.500"
-          isTruncated
-          letterSpacing="wider"
-        >
-          {blogDescription}
-        </Text>
+          <Text
+            width="85%"
+            textAlign="justify"
+            pb="5"
+            color="gray.500"
+            isTruncated
+            letterSpacing="wider"
+          >
+            {blogDescription}
+          </Text>
 
-        <Text variant="date" color="green.700">
-          {publishedAt}
-        </Text>
-      </Flex>
-    </Box>
+          <Text variant="date" color="green.700">
+            {publishedAt}
+          </Text>
+        </Flex>
+      </Box>
+      <Divider mt="10"></Divider>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,29 @@
 import Landing from "../Components/Landing";
+import Head from "next/head";
 export default function Home() {
   return (
-    <div>
+    <>
+      <Head>
+        <title>🏠Homepage | Sauru Welcome You</title>
+        <meta
+          name="description"
+          content="Hi, Sauru welcome you to his porfolio,in here he post his  blogs,projects,and anything he feels right 😁"
+        />
+        <meta property="og:title" content="Saurav Ghimire" />
+        <meta property="og:description" content="Welcome to my portfolio 👩‍💻" />
+        <meta
+          property="og:url"
+          content="https://portfolio-jet-gamma.vercel.app/"
+        />
+
+        <meta property="og:type" content="website" />
+
+        <link
+          rel="icon"
+          href="https://cdn-icons-png.flaticon.com/512/2881/2881142.png"
+        />
+      </Head>
       <Landing />
-    </div>
+    </>
   );
 }
