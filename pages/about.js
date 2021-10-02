068d1615bbacad73322272a13017ed3445d775b2
gsauarv <gsaurav2000@gmail.com>
2021-10-02 06:49:58 +0545

    some changes

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,23 @@
 // Container from chakra ui
 import { Container } from "@chakra-ui/layout";
+import Head from "next/head";
 // Importing the about components.
 import AboutComponent from "../Components/AboutComponent";
 
 const About = () => {
   return (
     <>
+      <Head>
+        <title>About Saurav</title>
+        <meta
+          name="description"
+          content="In this page you can learn more things about Saurav Ghimire."
+        ></meta>
+        <link
+          rel="icon"
+          href="https://cdn-icons-png.flaticon.com/512/2881/2881142.png"
+        />
+      </Head>
       <AboutComponent />
     </>
   );
